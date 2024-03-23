ad2e09d404532e9a7f2c8743aaa24cbaf1abccf1
tayebyassine <tayeb1920@gmail.com>
2024-03-23 15:55:17 +0100

    CSS modified

--- src/views/Sign-inSign-up.vue
+++ src/views/Sign-inSign-up.vue
@@ -1,48 +1,50 @@
 <template>
   <div class="authentication-page">
     <div class="login-page">
-      <div class="login-popup">
-        <div class="toggle-buttons">
-          <button @click="switchToSignin" :class="{ active: currentForm === 'signin' }">Sign In</button>
-          <button @click="switchToSignup" :class="{ active: currentForm === 'signup' }">Sign Up</button>
-        </div>
-        <div class="login-content">
-          <div v-if="currentForm === 'signin'" class="form-section">
-            <CustomForm :isSignup="false" />
-          </div>
-          <div v-else class="form-section">
-            <CustomForm :isSignup="true" />
-          </div>
-        </div>
-      </div>
-      <div class="footer-section">
-        <div class="col-lg-12 no-pdd">
-          <div v-if="currentForm === 'signin'" class="remember-me-section">
-            <div class="checkbox-section">
-              <input type="checkbox" name="remember" id="remember-checkbox">
-              <label for="remember-checkbox">
-                <span></span>
-              </label>
-              <small>Remember me</small>
+      <div class="login-content">
+        <div class="form-column">
+          <div class="login-popup">
+            <div class="toggle-buttons">
+              <button @click="switchToSignin" :class="{ active: currentForm === 'signin' }">Sign In</button>
+              <button @click="switchToSignup" :class="{ active: currentForm === 'signup' }">Sign Up</button>
             </div>
-            <a href="#" class="forgot-password-link" title="">Forgot Password?</a>
-          </div>
-        </div>
-        <div class="col-lg-12 no-pdd">
-          <div class="agree-terms-section">
-            <div v-if="currentForm === 'signup'" class="checkbox-section">
-              <input type="checkbox" name="agree-terms" id="agree-terms-checkbox">
-              <label for="agree-terms-checkbox">
-                <span></span>
-              </label>
-              <small>Yes, I understand and agree to the terms & conditions.</small>
+            <transition name="fade" mode="out-in">
+              <div :key="currentForm" class="form-section">
+                <div v-if="currentForm === 'signin'">
+                  <CustomForm :isSignup="false" />
+                </div>
+                <div v-else>
+                  <CustomForm :isSignup="true" />
+                </div>
+                <div class="col-lg-12 no-pdd">
+          
+            <div v-if="currentForm === 'signin'" class="remember-me-section" key="signin-remember">
+              <div class="checkbox-section">
+                <input type="checkbox" name="remember" id="remember-checkbox">
+                <label for="remember-checkbox">
+                  <span></span>
+                </label>
+                <small>Remember me</small>
+              </div>
+              <a href="#" class="forgot-password-link" title="">Forgot Password?</a>
             </div>
-          </div>
         </div>
-        <div class="container">
-          <ul>
-            <li><a href="ContactUs.html" title="">Contact us</a></li>
-          </ul>
+              </div>
+            </transition>
+          </div>
+
+    
+        </div>
+        <div class="about-column">
+          <div class="about-section">
+            <h2>About Us</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut
+              purus eget sapien egestas fermentum. Nullam nec nunc nec nunc
+              fermentum.
+            </p>
+            <img src="../../public/img/logo.png" alt="About Us" style="width: 200px; height: auto;" />
+          </div>
         </div>
       </div>
     </div>
@@ -72,26 +74,52 @@ export default {
 };
 </script>
 
-
 <style scoped>
 .authentication-page {
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh;
-  background: #f6F0ED;
+  background: #f6f0ed;
+}
+.col-lg-12 {
+  margin-top: 10px;
 }
 
 .login-page {
   width: 100%;
-  max-width: 400px;
-  background: #BBB193;
-  border-radius: 5px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  max-width: 800px; 
+  display: flex;
+  flex-direction: column;
+  background: #eaeaea; 
+  border-radius: 10px; 
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1); 
 }
+.login-content {
+  display: flex;
+}
+
+.form-column {
+  flex: 1;
+}
+
+.about-column {
+  flex: 1;
+}
+
+.about-section {
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+}
+
 
 .login-popup {
   padding: 30px;
+  height: 690px;
+  overflow: auto;
 }
 
 .toggle-buttons {
@@ -104,21 +132,26 @@ export default {
   padding: 10px 20px;
   border: none;
   background: transparent;
-  color: #28536B;
-  font-size: 16px;
+  color: #28536b;
+  font-size: 23px;
   cursor: pointer;
   outline: none;
+  border-radius: 5px; 
+  transition: background-color 0.3s ease; 
 }
 
-
 .toggle-buttons button.active {
-  color: #C2948A;
+  color: #c2948a;
+}
+
+.toggle-buttons button:hover {
+  background-color: #dcdcdc; 
 }
 
 .login-content {
   padding: 20px;
   border: 1.5px solid #f1f1f1;
-  border-radius: 5px;
+  border-radius: 10px; 
 }
 
 .form-section {
@@ -129,7 +162,6 @@ export default {
   padding: 20px;
   border-top: 1px solid #f1f1f1;
   background: #f9f9f9;
-  border-radius: 0 0 5px 5px;
 }
 
 .remember-me-section {
@@ -145,7 +177,7 @@ export default {
 }
 
 .checkbox-section input[type="checkbox"] {
-  display: none; 
+  display: none;
 }
 
 .checkbox-section label {
@@ -153,17 +185,17 @@ export default {
   display: inline-flex;
   align-items: center;
   justify-content: center;
-  width: 15px; 
-  height: 15px; 
-  background-color: #f5f5f5; 
+  width: 15px;
+  height: 15px;
+  background-color: #f5f5f5;
   border: 2px solid #d8d8d8;
-  border-radius: 4px; 
-  cursor: pointer; 
-  transition: background-color 0.2s ease-in-out, border-color 0.2s ease-in-out; 
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.2s ease-in-out, border-color 0.2s ease-in-out;
 }
 
 .checkbox-section input[type="checkbox"]:checked + label {
-  background-color: #007bff; 
+  background-color: #007bff;
   border-color: #007bff;
 }
 
@@ -174,19 +206,12 @@ export default {
   font-size: 14px;
   font-weight: bold;
   opacity: 0;
-  transition: opacity 0.2s ease-in-out; 
+  transition: opacity 0.2s ease-in-out;
 }
 
 .checkbox-section input[type="checkbox"]:checked + label::after {
-  opacity: 1; 
+  opacity: 1;
 }
-
-
-
-
-
-
-
 
 .forgot-password-link {
   color: #007bff;
@@ -194,76 +219,16 @@ export default {
   text-decoration: none;
 }
 
-.agree-terms-section {
-  display: flex;
-  align-items: center;
-}
-
-.agree-terms-section input[type="checkbox"] {
-  display: none; 
-}
-
-.agree-terms-section label {
-  position: relative;
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  width: 15px; 
-  height: 15px; 
-  background-color: #f5f5f5; 
-  border: 2px solid #d8d8d8;
-  border-radius: 4px; 
-  cursor: pointer; 
-  transition: background-color 0.2s ease-in-out, border-color 0.2s ease-in-out; 
-}
-
-.agree-terms-section input[type="checkbox"]:checked + label {
-  background-color: #007bff; 
-  border-color: #007bff;
-}
-
-.agree-terms-section label::after {
-  content: "\2713";
-  font-family: Arial, sans-serif;
-  color: white;
-  font-size: 14px;
-  font-weight: bold;
-  opacity: 0;
-  transition: opacity 0.2s ease-in-out; 
-}
-
-.agree-terms-section input[type="checkbox"]:checked + label::after {
-  opacity: 1; 
-}
-
-
-
-.container {
-  display: flex;
-  justify-content: center;
-}
-
-.container ul {
-  display: flex;
-  list-style: none;
-  padding: 0;
-}
-
-.container ul li {
-  margin-right: 10px;
-}
-
-.container ul li a {
-  color: #007bff;
-  text-decoration: none;
-}
-
-.container ul li a:hover {
-  text-decoration: underline;
-}
 
 .no-pdd {
   padding: 0;
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.3s ease-in-out;
+}
+
+.fade-enter, .fade-leave-to {
+  opacity: 0;
 }
 
 @media (max-width: 767px) {
@@ -300,16 +265,10 @@ export default {
     align-items: flex-start;
     font-size: 30px;
   }
-
-  .agree-terms-section {
+.container {
     flex-direction: column;
-    align-items: flex-start;
+    align-items: center;
   }
-
-  .container {
-    padding: 10px;
-  }
+  
 }
-
-
 </style>
