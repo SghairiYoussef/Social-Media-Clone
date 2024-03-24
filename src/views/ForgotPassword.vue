961191de94199174205036acaa98ad3172125114
tayebyassine <tayeb1920@gmail.com>
2024-03-24 01:45:53 +0100

    Minor fixes to the Forgot Password page

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,16 +1,18 @@
 <template>
-    <div class="forgotPassword-page">
-      <div class="forgotPassword-container">
+  <div class="forgotPassword-page">
+    <div class="forgotPassword-container">
+      <form @submit.prevent="submitForm">
         <div class="forgotPassword-content">
           <p class="forgotPassword-notification">Please enter your email</p>
           <CustomInput label="Email" type="email" v-model="email" />
         </div>
         <div class="submit-btn">
-          <button type="submit" @click="submitForm">Submit</button>
+          <button type="submit">Submit</button>
         </div>
-      </div>
+      </form>
     </div>
-  </template>
+  </div>
+</template>
   
   <script>
   import CustomInput from '@/components/Authentification/CustomInput.vue';
