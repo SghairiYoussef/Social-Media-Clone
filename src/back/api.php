ca851a26ba5db7f7b4f1382f8f9f1ea5cb5d7c9c
youssef <youssefaridhi8@gmail.com>
2024-03-25 13:58:41 +0100

    Integrate database retrieval of comments for posts

--- src/back/api.php
+++ src/back/api.php
@@ -6,6 +6,7 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 include "function.php";
 include "getPosts.php";
 
+
 $action='';
 if (isset($_GET['action'])) {
 
---
+++ src/back/getComments.php
@@ -0,0 +1,19 @@
+<?php
+    require_once "DataBase.php";
+    function getComments($Post_ID){
+          
+          $connexion = ConnexionBD::getInstance();
+          $Post_ID = $connexion->quote($Post_ID);
+        $query="select U.User_ID, U.Username, U.image, C.* from comment C
+        inner join profile U on U.User_ID = C.User_ID
+        where C.Post_ID = $Post_ID
+        order by C.Date_posted desc";
+        $result = $connexion->query($query);
+        $comments = array();
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            $comments[] = $row;
+        }
+        return $comments;
+    }
+    
+?>
