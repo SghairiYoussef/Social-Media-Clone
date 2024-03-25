5f23a92e0d844d88b8607327f2ea88e396432083
tayebyassine <tayeb1920@gmail.com>
2024-03-26 00:55:22 +0100

    sendEmail.php added + verification.php modified

--- src/back/verification.php
+++ src/back/verification.php
@@ -1,10 +1,5 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\SMTP;
-
-//Load Composer's autoloader
-require 'C:\xampp\htdocs\php\Social-Media-Clone\vendor\autoload.php';
+require 'sendEmail.php';
 
 function generateVerificationCode()
 {
@@ -20,41 +15,13 @@ function generateVerificationCode()
 
 function sendVerificationEmail($email, $verificationCode)
 {
-    $mail = new PHPMailer(true);
-
-    try {
-        //Server settings
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';
-        $mail->SMTPAuth = true;
-        $mail->Username = 'youssef.fazloun@gmail.com';
-        $mail->Password = 'ffdw efhd xfqu osxe';
-
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port = 587;
-
-
-        //Recipients
-        $mail->setFrom('youssef.fazloun@gmail.com', 'Mailer');
-        $mail->addAddress($email);
-
-
-        //Content
-        $mail->isHTML(true);
-        $mail->Subject = 'Here is the subject';
-        $mail_template = "
-    <h1>Verification</h1>
-    <p>Your verification code is: $verificationCode</p>
-    <br>
-    <p>Put this code in the verification form to complete the registration process</p> 
-        ";
-        $mail->Body = $mail_template;
-
-        $mail->send();
-        return true;
-    }
-    catch (Exception $e) {
-        return false;
-    }
+    $subject = "Verification";
+    $body = "
+        <h1>Verification</h1>
+        <p>Your verification code is: $verificationCode</p>
+        <br>
+        <p>Put this code in the verification form to complete the registration process</p>
+    ";
+    return sendEmail($email, $subject, $body);
 }
 ?>
