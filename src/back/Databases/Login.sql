e47f95fdda1d8e6f57e74fae29b149bb1855539a
Kacem Mathlouthi <kacem.math47@gmail.com>
2024-03-28 21:05:53 +0100

    Fixed Login.sql and added README File

--- src/back/Databases/Login.sql
+++ src/back/Databases/Login.sql
@@ -1,18 +1,14 @@
 -- Run this script in phpmyadmin SQL section to create the database and test --
 
-CREATE DATABASE IF NOT EXISTS login;
-
-USE login;
-
-CREATE TABLE IF NOT EXISTS users (
-    userName VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    fullName VARCHAR(255) NOT NULL,
-    birthDay DATE NOT NULL,
-    PRIMARY KEY (userName, email)
-    );
-
-insert into users (userName, email, password, fullName, birthDay) values ('admin', 'admin@gl2.com', 'admin', 'admin', '1999-01-01');
-insert into users (userName, email, password, fullName, birthDay) values ('user', 'user@gl2.com', 'user', 'user', '1999-01-01');
-```
+CREATE OR REPLACE TABLE userdata (
+    userID INT AUTO_INCREMENT PRIMARY KEY,
+    fullName VARCHAR(64),
+    userName VARCHAR(32),
+    email VARCHAR(64),
+    password VARCHAR(64),
+    birthDay DATE,
+    resetPasswordToken VARCHAR(64),
+    rememberMeToken VARCHAR(64),
+    bio VARCHAR(256),
+    image VARCHAR(126)
+);
