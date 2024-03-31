183357baf29cf2afff0262e5ef99e780afca6773
tayebyassine <tayeb1920@gmail.com>
2024-03-31 17:15:18 +0100

    fixed selectUser

--- src/views/Messages.vue
+++ src/views/Messages.vue
@@ -2,14 +2,14 @@
     <navBar/>
     <header>
         <h1>Messages</h1>
-        <h2>{{ Name }}</h2>
+        <h2>{{ selectedUser?.name }}</h2>
         <p>{{ status }}</p> <!-- Display user status (online wala le) here  -->
     </header>
     <div class="row">
         <div class="col-sm-4">
             <Inbox :users="users" @user-selected="selectUser"/>
         </div>
-        <div v-if="activeContactIndex === null" class="Note">
+        <div v-if="!selectedUser" class="Note">
             Select a contact to start chatting
         </div>
         <div v-else class="col-sm-8">
@@ -36,31 +36,17 @@ export default {
         return{
             status: 'Online',
             search: '',
-            users: [
-                {
-                    id: 1,
-                    name: 'User 1',
-                    unread_messages: 2
-                },
-                {
-                    id: 2,
-                    name: 'User 2',
-                    unread_messages: 0
-                },
-                {
-                    id: 3,
-                    name: 'User 3',
-                    unread_messages: 1
-                }
-            ],
-            activeContactIndex: null
+            users: [],
+            selectedUser: null
         }
     },
 
     methods: {
         selectUser(user)
         {
-            this.activeContactIndex = this.users.indexOf(user);
+            this.selectedUser = user;
+            console.log(this.selectedUser);
+            console.log(this.users);
         },
     }
 };
