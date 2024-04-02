41d9d730691617938ae787b8c0cf91e54dc46bbc
youssef <youssefaridhi8@gmail.com>
2024-04-02 15:45:08 +0200

    added add and remove react features

--- src/back/Controllers/addReact.php
+++ src/back/Controllers/addReact.php
@@ -1,35 +1,27 @@
 <?php
     require_once "./DataBase.php";
-    function addReact($post_id,$user_id){
+    function addReact($post_id, $user_id) {
         $connexion = ConnexionBD::getInstance();
         $post_id = $connexion->quote($post_id);
-        //add to reacts table
-        $query="insert into reacts(User_ID, Post_ID) values ($user_id, $post_id)";
-        $result = $connexion->exec($query);
-        if($result){
-            //get the current react count
-        $sql = "SELECT React_Count FROM post WHERE Post_ID = $post_id";
-        $result = $connexion->query($sql);
-        $reactCount = $result->fetch(PDO::FETCH_ASSOC);
-        $reactCount = $reactCount['React_Count'];
-        $reactCount++;
-        //update react count
-        $query="UPDATE post
-        SET React_Count = $reactCount
-        WHERE Post_ID = $post_id";
-        $result = $connexion->exec($query);
-        //add to reacts table
-        $query="insert into reacts(User_ID, Post_ID) values ($user_id, $post_id)";
-        $result = $connexion->exec($query);
-        if($result){
+        $user_id = $connexion->quote($user_id);
+    
+        $query = "SELECT * FROM reacts WHERE User_ID = $user_id AND Post_ID = $post_id";
+        $result = $connexion->query($query);
+    
+        if ($result->rowCount() == 0) {
+            $query = "INSERT INTO reacts (User_ID, Post_ID) VALUES ($user_id, $post_id)";
+            $connexion->exec($query);
+    
+            $query = "UPDATE post SET React_Count = React_Count + 1 WHERE Post_ID = $post_id";
+            $connexion->exec($query);
             return true;
-        }else{
-            return false;
+        } else { 
+            $query = "DELETE FROM reacts WHERE User_ID = $user_id AND Post_ID = $post_id";
+            $connexion->exec($query);    
+            $query = "UPDATE post SET React_Count = React_Count - 1 WHERE Post_ID = $post_id";
+            $connexion->exec($query);
+            return true;
         }
-        }else{
-            return false;
-        }
-        
     }
     
 ?>
