e7c3ff901a742515d81a5101799c36b28514ad34
youssef <youssefaridhi8@gmail.com>
2024-04-02 17:29:52 +0200

    removed uploading image functionality until futher notice

--- src/DataBases/post.sql
+++ src/DataBases/post.sql
@@ -3,7 +3,7 @@ CREATE OR REPLACE TABLE post (
     Caption VARCHAR(500),
     Date_published DATE DEFAULT CURRENT_DATE() ,
     Media VARCHAR(500),
-    React_Count INT,
+    React_Count INT DEFAULT 0,
     User_ID INT,
     title VARCHAR(64)
 );
