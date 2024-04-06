d46ae6523a3fdc91a09ed1b61cb9748639e20fdb
tayebyassine <tayeb1920@gmail.com>
2024-04-06 15:43:58 +0100

    fixed a bug in database script

--- src/DataBase Script/script.sql
+++ src/DataBase Script/script.sql
@@ -9,7 +9,7 @@ CREATE OR REPLACE TABLE userdata (
     email VARCHAR(64),
     password VARCHAR(64),
     birthDay DATE,
-    userStatus VARCHAR(64) DEFAULT 'offline',
+    userStatus VARCHAR(64) DEFAULT 'Offline',
     bio VARCHAR(256),
     img VARCHAR(512),
     resetPasswordToken VARCHAR(64),
